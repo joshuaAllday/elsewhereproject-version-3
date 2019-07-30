ca37663e9e7c7f1fe24381392a024a78b8a945ba
Joshua Allday <joshallday@icloud.com>
2019-07-30 14:55:23 +0100

    adding lazy loading and suspense to pages

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {Fragment, useEffect} from 'react';
+import React, {Fragment, useEffect, lazy, Suspense} from 'react';
 import { connect } from 'react-redux';
 import { Switch, Route } from 'react-router-dom';
 
@@ -6,12 +6,14 @@ import { fetchCollectionsStartAsync } from './redux/articles/articles.actions';
 
 import Header from './components/header/header.component';
 import LandingPage from './pages/landing-page/landing-page.component';
-import AboutPage from './pages/about-page/about-page.component';
-import ArticlePage from './pages/article-page/article-page.component';
-import PostPage from './pages/post-page/post-page.component';
-import MapPage from './pages/map-page/map-page.component';
+import Spinner from './components/spinner/spinner.component';
 
 import './App.css';
+
+const AboutPage = lazy(() => import('./pages/about-page/about-page.component'));
+const ArticlePage = lazy(() => import('./pages/article-page/article-page.component'));
+const PostPage = lazy(() => import('./pages/post-page/post-page.component'));
+const MapPage = lazy(() => import('./pages/map-page/map-page.component'));
 
 const App = ({fetchCollectionsStartAsync}) => {
 
@@ -22,14 +24,16 @@ const App = ({fetchCollectionsStartAsync}) => {
   return(
     <div className="App">
       <Switch>
-        <Route exact path='/map' component={MapPage} />
-        <Fragment>
-          <Header />
-          <Route exact path='/' component={LandingPage} />
-          <Route exact path='/about' component={AboutPage} />
-          <Route exact path='/article/:id' component={ArticlePage} />
-          <Route exact path='/post' component={PostPage} />
-        </Fragment>
+        <Suspense fallback={<Spinner />}>
+          <Route exact path='/map' component={MapPage} />
+          <Fragment>
+            <Header />
+            <Route exact path='/' component={LandingPage} />
+            <Route exact path='/about' component={AboutPage} />
+            <Route exact path='/article/:id' component={ArticlePage} />
+            <Route exact path='/post' component={PostPage} />
+          </Fragment>
+        </Suspense>
       </Switch>
     </div>
   );
