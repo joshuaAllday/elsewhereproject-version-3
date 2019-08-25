585bf066a8ded06465459265ff196b70bd02c728
Joshua Allday <joshallday@icloud.com>
2019-08-25 17:07:20 +0200

    updated packages

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,5 @@
 import { createStore, applyMiddleware } from 'redux';
-// import logger from 'redux-logger';
+import logger from 'redux-logger';
 import thunk from 'redux-thunk';
 import { persistStore } from 'redux-persist';
 
@@ -7,9 +7,9 @@ import rootReducer from './root-reducer';
 
 const middlewares = [thunk];
 
-// if (process.env.NODE_ENV === 'development'){
-//     middlewares.push(logger)
-// }
+if (process.env.NODE_ENV === 'development'){
+    middlewares.push(logger)
+}
 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
